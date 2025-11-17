d236273458a875e1187ff61276ef4cee3bd0a12b
Kinorw <otherlittle66@gmail.com>
2025-11-17 18:22:26 +0800

    feat: asst1_prog1,2

--- asst1-main/prog2_vecintrin/CMU418intrin.h
+++ asst1-main/prog2_vecintrin/CMU418intrin.h
@@ -1,5 +1,5 @@
 // Define vector unit width here
-#define VECTOR_WIDTH 8
+#define VECTOR_WIDTH 16
 
 #ifndef CMU418INTRIN_H_
 #define CMU418INTRIN_H_
@@ -20,12 +20,15 @@ struct __cmu418_vec {
 };
 
 // Declare a mask with __cmu418_mask
+// 中文： 使用cmu418_mask声明一个掩码
 struct __cmu418_mask : __cmu418_vec<bool> {};
 
 // Declare a floating point vector register with __cmu418_vec_float
+// 中文： 使用cmu418_vec_float声明一个浮点向量寄存器
 #define __cmu418_vec_float __cmu418_vec<float>
 
 // Declare an integer vector register with __cmu418_vec_int
+// 中文： 使用cmu418_vec_int声明一个整数向量寄存器
 #define __cmu418_vec_int   __cmu418_vec<int>
 
 //***********************
@@ -33,9 +36,11 @@ struct __cmu418_mask : __cmu418_vec<bool> {};
 //***********************
 
 // Return a mask initialized to 1 in the first N lanes and 0 in the others
+// 返回一个掩码，其前N个通道初始化为1，其他通道初始化为0
 __cmu418_mask _cmu418_init_ones(int first = VECTOR_WIDTH);
 
 // Return the inverse of maska
+// 返回maska的反转
 __cmu418_mask _cmu418_mask_not(__cmu418_mask &maska);
 
 // Return (maska | maskb)
@@ -45,38 +50,46 @@ __cmu418_mask _cmu418_mask_or(__cmu418_mask &maska, __cmu418_mask &maskb);
 __cmu418_mask _cmu418_mask_and(__cmu418_mask &maska, __cmu418_mask &maskb);
 
 // Count the number of 1s in maska
+// 返回maska中1的数量
 int _cmu418_cntbits(__cmu418_mask &maska);
 
 // Set register to value if vector lane is active
 //  otherwise keep the old value
+// 设置寄存器的值（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vset_float(__cmu418_vec_float &vecResult, float value, __cmu418_mask &mask);
 void _cmu418_vset_int(__cmu418_vec_int &vecResult, int value, __cmu418_mask &mask);
 // For user's convenience, returns a vector register with all lanes initialized to value
+// 方便用户使用，返回一个所有通道都初始化为value的向量寄存器
 __cmu418_vec_float _cmu418_vset_float(float value);
 __cmu418_vec_int _cmu418_vset_int(int value);
 
 // Copy values from vector register src to vector register dest if vector lane active
 // otherwise keep the old value
+// 从向量寄存器src复制值到向量寄存器dest（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vmove_float(__cmu418_vec_float &dest, __cmu418_vec_float &src, __cmu418_mask &mask);
 void _cmu418_vmove_int(__cmu418_vec_int &dest, __cmu418_vec_int &src, __cmu418_mask &mask);
 
 // Load values from array src to vector register dest if vector lane active
 //  otherwise keep the old value
+// 从数组src加载值到向量寄存器dest（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vload_float(__cmu418_vec_float &dest, float* src, __cmu418_mask &mask);
 void _cmu418_vload_int(__cmu418_vec_int &dest, int* src, __cmu418_mask &mask);
 
 // Store values from vector register src to array dest if vector lane active
 //  otherwise keep the old value
+// 从向量寄存器src存储值到数组dest（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vstore_float(float* dest, __cmu418_vec_float &src, __cmu418_mask &mask);
 void _cmu418_vstore_int(int* dest, __cmu418_vec_int &src, __cmu418_mask &mask);
 
 // Return calculation of (veca + vecb) if vector lane active
 //  otherwise keep the old value
+// 返回（veca + vecb）的计算结果（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vadd_float(__cmu418_vec_float &vecResult, __cmu418_vec_float &veca, __cmu418_vec_float &vecb, __cmu418_mask &mask);
 void _cmu418_vadd_int(__cmu418_vec_int &vecResult, __cmu418_vec_int &veca, __cmu418_vec_int &vecb, __cmu418_mask &mask);
 
 // Return calculation of (veca - vecb) if vector lane active
 //  otherwise keep the old value
+// 返回（veca - vecb）的计算结果（如果向量通道处于活动状态），否则保持旧值
 void _cmu418_vsub_float(__cmu418_vec_float &vecResult, __cmu418_vec_float &veca, __cmu418_vec_float &vecb, __cmu418_mask &mask);
 void _cmu418_vsub_int(__cmu418_vec_int &vecResult, __cmu418_vec_int &veca, __cmu418_vec_int &vecb, __cmu418_mask &mask);
 
@@ -120,14 +133,17 @@ void _cmu418_veq_int(__cmu418_mask &vecResult, __cmu418_vec_int &veca, __cmu418_
 
 // Adds up adjacent pairs of elements, so
 //  [0 1 2 3] -> [0+1 0+1 2+3 2+3]
+// 中文：相邻元素两两相加，
 void _cmu418_hadd_float(__cmu418_vec_float &vecResult, __cmu418_vec_float &vec);
 
 // Performs an even-odd interleaving where all even-indexed elements move to front half
 //  of the array and odd-indexed to the back half, so
 //  [0 1 2 3 4 5 6 7] -> [0 2 4 6 1 3 5 7]
+// 中文：执行偶数-奇数交错，所有偶数索引元素移动到数组的前半部分，奇数索引元素移动到后半部分，
 void _cmu418_interleave_float(__cmu418_vec_float &vecResult, __cmu418_vec_float &vec);
 
 // Add a customized log to help debugging
+// 为了帮助调试，添加一个自定义日志
 void addUserLog(const char * logStr);
 
 #endif
--- asst1-main/prog2_vecintrin/functions.cpp
+++ asst1-main/prog2_vecintrin/functions.cpp
@@ -8,12 +8,12 @@ using namespace std;
 
 void absSerial(float* values, float* output, int N) {
     for (int i=0; i<N; i++) {
-	float x = values[i];
-	if (x < 0) {
-	    output[i] = -x;
-	} else {
-	    output[i] = x;
-	}
+		float x = values[i];
+		if (x < 0) {
+			output[i] = -x;
+		} else {
+			output[i] = x;
+		}
     }
 }
 
@@ -60,36 +60,71 @@ void absVector(float* values, float* output, int N) {
 // 4.18.  Store result in outputs.
 // Uses iterative squaring, so that total iterations is proportional
 // to the log_2 of the exponent
+/* 中文：接收一个值数组和一个指数数组
+ 对于每个元素，计算values[i]^exponents[i]并将值夹紧到4.18。
+ 将结果存储在输出中。
+ 使用迭代平方，因此总迭代次数与指数的log_2成正比 
+*/
 void clampedExpSerial(float* values, int* exponents, float* output, int N) {
     for (int i=0; i<N; i++) {
-	float x = values[i];
-	float result = 1.f;
-	int y = exponents[i];
-	float xpower = x;
-	while (y > 0) {
-	    if (y & 0x1) {
-			result *= xpower;
+		float x = values[i];
+		float result = 1.f;
+		int y = exponents[i];
+		float xpower = x;
+		while (y > 0) {
+			if (y & 0x1) {
+				result *= xpower;
+			}
+			xpower = xpower * xpower;
+			y >>= 1;
 		}
-	    xpower = xpower * xpower;
-	    y >>= 1;
-	}
-	if (result > 4.18f) {
-	    result = 4.18f;
-	}
-	output[i] = result;
+		if (result > 4.18f) {
+			result = 4.18f;
+		}
+		output[i] = result;
     }
 }
 
 void clampedExpVector(float* values, int* exponents, float* output, int N) {
     // Implement your vectorized version of clampedExpSerial here
     //  ...
+	__cmu418_vec_int vecZero = _cmu418_vset_int(0),vecOne = _cmu418_vset_int(1);
+	__cmu418_vec_float vec418 = _cmu418_vset_float(4.18f);
+	for(int i = 0 ; i < N ; i += VECTOR_WIDTH){
+		int width = min(VECTOR_WIDTH,N - i);
+		__cmu418_mask vecMaskOne = _cmu418_init_ones(width);
+		
+		__cmu418_vec_float x;
+		_cmu418_vload_float(x,values+i,vecMaskOne);
+		__cmu418_vec_int y;
+		_cmu418_vload_int(y,exponents+i,vecMaskOne);
+
+		__cmu418_vec_float vecResult = _cmu418_vset_float(1.f);
+		__cmu418_mask activate = _cmu418_init_ones(0);
+		__cmu418_mask v418 = _cmu418_mask_not(vecMaskOne);
+		
+		_cmu418_vgt_int(activate,y,vecZero,vecMaskOne);
+		while(_cmu418_cntbits(activate)){
+			__cmu418_vec_int vba ;
+			__cmu418_mask vbaMask = vbaMask = _cmu418_init_ones(0);
+			_cmu418_vbitand_int(vba,y,vecOne,activate);
+			_cmu418_veq_int(vbaMask,vba,vecOne,activate);
+			_cmu418_vmult_float(vecResult,vecResult,x,vbaMask);
+			_cmu418_vmult_float(x,x,x,activate);
+			_cmu418_vshiftright_int(y,y,vecOne,activate);
+			_cmu418_vgt_int(activate,y,vecZero,vecMaskOne);	
+		}
+		_cmu418_vgt_float(v418,vecResult,vec418,vecMaskOne);
+		_cmu418_vset_float(vecResult,4.18f,v418);
+		_cmu418_vstore_float(output+i,vecResult,vecMaskOne);
+	}
 }
 
 
 float arraySumSerial(float* values, int N) {
     float sum = 0;
     for (int i=0; i<N; i++) {
-	sum += values[i];
+		sum += values[i];
     }
 
     return sum;
@@ -97,8 +132,28 @@ float arraySumSerial(float* values, int N) {
 
 // Assume N % VECTOR_WIDTH == 0
 // Assume VECTOR_WIDTH is a power of 2
+// 假设N % VECTOR_WIDTH == 0
+// 假设VECTOR_WIDTH是2的幂
 float arraySumVector(float* values, int N) {
     // Implement your vectorized version here
     //  ...
-	return 0.f;
+	__cmu418_vec_float vsum = _cmu418_vset_float(0.f);
+	for(int i = 0 ; i < N ; i += VECTOR_WIDTH){
+		__cmu418_mask mask = _cmu418_init_ones(min(VECTOR_WIDTH,N - i));
+		__cmu418_vec_float x;
+		_cmu418_vload_float(x,values+i,mask);
+		_cmu418_vadd_float(vsum,vsum,x,mask);
+	}
+	for(int i = VECTOR_WIDTH ; i > 1 ; i >>=1){
+		_cmu418_hadd_float(vsum,vsum);
+		_cmu418_interleave_float(vsum,vsum);
+		// for(int j = 0 ; j < i ; ++ j){
+		// 	printf("%f ",vsum.value[j]);
+		// }
+		// printf("\n");
+	}
+	float sum = 0;
+	__cmu418_mask mask = _cmu418_init_ones(1);
+	_cmu418_vstore_float(&sum,vsum,mask);
+	return sum;
 }
